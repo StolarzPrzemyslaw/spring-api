59c2423a41a64e4f2c050619af146158e451bb38
StolarzPrzemyslaw <stolarz.przemyslaw@gmail.com>
2021-03-08 16:55:28 +0100

    Add DB_Schema.sql and init_schema.sql

---
+++ SQL/DB_Schema.sql
@@ -0,0 +1,43 @@
+DROP TABLE IF EXISTS category CASCADE;
+CREATE TABLE category (
+      id SERIAL,
+      name text,
+      PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS orders CASCADE;
+CREATE TABLE orders (
+  id SERIAL,
+  order_date date,
+  PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS product CASCADE;
+CREATE TABLE product (
+       id SERIAL,
+       name TEXT,
+       price numeric,
+       category_id int,
+       PRIMARY KEY (id)
+);
+
+ALTER TABLE ONLY product
+    ADD CONSTRAINT category_id FOREIGN KEY (category_id)
+    REFERENCES category(id) ON DELETE CASCADE;
+
+DROP TABLE IF EXISTS product_basket CASCADE;
+CREATE TABLE product_basket (
+  id SERIAL,
+  product_id int,
+  amount int,
+  order_id int,
+  PRIMARY KEY (id)
+);
+
+ALTER TABLE ONLY product_basket
+    ADD CONSTRAINT product_id FOREIGN KEY (product_id)
+    REFERENCES product(id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY product_basket
+    ADD CONSTRAINT order_id FOREIGN KEY (order_id)
+    REFERENCES orders(id) ON DELETE CASCADE;
