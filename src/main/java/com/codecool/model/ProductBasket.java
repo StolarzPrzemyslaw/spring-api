53e23965c0e14811b17c3bf3b1c0eec7159a3539
StolarzPrzemyslaw <stolarz.przemyslaw@gmail.com>
2021-03-09 20:14:29 +0100

    Implement ProductBasket model

---
+++ src/main/java/com/codecool/model/ProductBasket.java
@@ -0,0 +1,68 @@
+package com.codecool.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product_basket")
+public class ProductBasket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "amount")
+    private int amount;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    protected ProductBasket() {}
+
+    public ProductBasket(int amount, Product product, Order order) {
+        this.amount = amount;
+        this.product = product;
+        this.order = order;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("ProductBasket[id=%d, amount=%d, product=%s, order=%s]",
+                id, amount, product, order);
+    }
+}
