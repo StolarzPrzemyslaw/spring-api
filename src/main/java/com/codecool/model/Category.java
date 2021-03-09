aab4fcf0effd9bef4a8c8b009e8d3371a41e1e15
StolarzPrzemyslaw <stolarz.przemyslaw@gmail.com>
2021-03-09 18:41:44 +0100

    Implement Category model

---
+++ src/main/java/com/codecool/model/Category.java
@@ -0,0 +1,41 @@
+package com.codecool.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    protected Category() {}
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Category[id=%d, name='%s']", id, name);
+    }
+}
