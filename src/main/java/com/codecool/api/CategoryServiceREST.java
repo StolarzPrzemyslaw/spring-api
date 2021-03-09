a91329d1c525a53c399aa2c8e639010cd3fe3cc2
StolarzPrzemyslaw <stolarz.przemyslaw@gmail.com>
2021-03-10 00:10:06 +0100

    Implement "delete" method in category api

--- src/main/java/com/codecool/api/CategoryServiceREST.java
+++ src/main/java/com/codecool/api/CategoryServiceREST.java
@@ -43,4 +43,8 @@ public class CategoryServiceREST {
                 });
     }
 
+    @DeleteMapping("/categories/{id}")
+    void deleteEntity(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
 }
