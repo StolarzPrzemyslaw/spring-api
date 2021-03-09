6e76f2143ca4539705d901612f1d1bb5416bc734
StolarzPrzemyslaw <stolarz.przemyslaw@gmail.com>
2021-03-10 00:58:06 +0100

    Make clazz type in EntityNotFoundException.java generic

--- src/main/java/com/codecool/exception/EntityNotFoundException.java
+++ src/main/java/com/codecool/exception/EntityNotFoundException.java
@@ -2,7 +2,7 @@ package com.codecool.exception;
 
 
 public class EntityNotFoundException extends RuntimeException{
-    public EntityNotFoundException(Class clazz, Long id) {
+    public EntityNotFoundException(Class<?> clazz, Long id) {
         super(String.format("Could not find %s with id %d", clazz.getSimpleName(), id));
     }
 }
