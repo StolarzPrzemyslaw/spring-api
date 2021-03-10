5793c857ba7183c0041b5123f98c2175753c7570
StolarzPrzemyslaw <stolarz.przemyslaw@gmail.com>
2021-03-10 18:58:32 +0100

    Implement ProductBasketService in ProductBasketServiceREST.java

--- src/main/java/com/codecool/api/ProductBasketServiceREST.java
+++ src/main/java/com/codecool/api/ProductBasketServiceREST.java
@@ -1,52 +1,41 @@
 package com.codecool.api;
 
-import com.codecool.exception.EntityNotFoundException;
 import com.codecool.model.ProductBasket;
-import com.codecool.repository.ProductBasketRepository;
+import com.codecool.service.ProductBasketService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class ProductBasketServiceREST {
+    private final ProductBasketService productBasketService;
 
-    private final ProductBasketRepository repository;
-
-    public ProductBasketServiceREST(ProductBasketRepository repository) { this.repository = repository; }
+    public ProductBasketServiceREST(ProductBasketService productBasketService) {
+        this.productBasketService = productBasketService;
+    }
 
     @GetMapping("/api/productBasket")
     List<ProductBasket> all() {
-        return repository.findAll();
+        return productBasketService.readAll();
     }
 
     @PostMapping("/api/productBasket")
     ProductBasket newEntity(@RequestBody ProductBasket newEntity) {
-        return repository.save(newEntity);
+        return productBasketService.create(newEntity);
     }
 
     @GetMapping("/api/productBasket/{id}")
     ProductBasket one(@PathVariable Long id) {
-        return repository.findById(id).
-                orElseThrow(() -> new EntityNotFoundException(ProductBasket.class, id));
+        return productBasketService.read(id);
     }
 
     @PutMapping("/api/productBasket/{id}")
     ProductBasket replaceEntity(@RequestBody ProductBasket newEntity, @PathVariable Long id) {
-        return repository.findById(id).
-                map(entity -> {
-                    entity.setAmount(newEntity.getAmount());
-                    entity.setProduct(newEntity.getProduct());
-                    entity.setOrder(newEntity.getOrder());
-                    return repository.save(entity);
-                }).
-                orElseGet(() -> {
-                    newEntity.setId(id);
-                    return repository.save(newEntity);
-                });
+        return productBasketService.update(newEntity, id);
     }
 
     @DeleteMapping("/api/productBasket/{id}")
     void deleteEntity(@PathVariable Long id) {
-        repository.deleteById(id);
+        productBasketService.delete(id);
     }
 }
